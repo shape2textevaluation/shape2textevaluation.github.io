608082cea5987c6b08f1efa57f92660a3c12fd52
AndreAmaduzzi <amaduzziandrea@gmail.com>
2023-02-10 13:11:51 +0100

    update function.js

--- function.js
+++ function.js
@@ -59,12 +59,12 @@ function sampleImages(){
   base_url = "https://raw.githubusercontent.com/shape2textevaluation/shape2textevaluation.github.io/assets/"
   if (draping_modes[0]=="gt") 
       {
-        img0.src = base_url + draping_modes[0] + "/" + "0" + ".png"
+        img0.src = "https://raw.githubusercontent.com/shape2textevaluation/shape2textevaluation.github.io/assets/gt/0.png"
         img1.src = base_url + draping_modes[1] + "/" + "0_1" + ".png"
       }
   else 
       {
-        img1.src = base_url + draping_modes[0] + "/" + "0" + ".png"
+        img1.src = "https://raw.githubusercontent.com/shape2textevaluation/shape2textevaluation.github.io/assets/gt/0.png"
         img0.src = base_url + draping_modes[1] + "/" + "0_2" + ".png"
       }
 }
