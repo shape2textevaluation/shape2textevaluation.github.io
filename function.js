ced45257b79ce981260190b379adc35727cf13f4
AndreAmaduzzi <amaduzziandrea@gmail.com>
2023-02-15 16:53:31 +0100

    update Google Sheet URL and line to avoid favicon error

--- function.js
+++ function.js
@@ -208,7 +208,7 @@ function sendData(data) {
   //XHR.open('POST', 'https://script.google.com/macros/s/AKfycbzgoJLTCyaqrgrZ2jCHQZKymAZpW7ON1YX4nhXJ8lNyvraYf1MzJF01MIjJFWI-GQE/exec');
 
   // Set up request to my Google Sheet
-  XHR.open('POST', 'https://script.google.com/macros/s/AKfycbzJKxXXfkYJsXXaKZR6dau837x0DKo76M0GeMz1Up9lt-qonPuB0E9TXN_gS1AdI9Ua7Q/exec');
+  XHR.open('POST', 'https://script.google.com/macros/s/AKfycbyF6a7xDnqCM-GzVsOKYBiTEDfWX0hpiwWm-gdgHl3340I2_uSKmOHCVF7s4DThpWeu/exec');
 
   // Send our FormData object; HTTP headers are set automatically
   XHR.send(FD);
